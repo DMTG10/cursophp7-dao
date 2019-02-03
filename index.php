1266c426c73a251c9cd5475b7911a5370410de05
Diego Montenegro de Medeiros <diegomtgmdr@gmail.com>
2019-02-03 15:13:14 -0200

    classe updtae

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
 <?php
 require_once("config.php");
 
-$spl = new Sql();
+//carraga somente um usuario
+//$root = new Usuario();
+//$root->loadByID(3);
+//echo $root;
 
-$usuarios = $spl->select("SELECT * FROM tb_usuarios");
-echo json_encode($usuarios);
+
+//Carrega um lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+//Carrega uma lista de usuario buscando pelo login
+
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuario usando o login e senha
+//$usuario = new Usuario();
+//$usuario->logi//n("jose manolo", "4da54d5sa");
+//echo $usuari//o//;
+
+//inserir dados do banco 
+
+//$aluno = new Usuario("VAldemir", "nosta1235");
+//$aluno-> insert();
+//echo $aluno;
+
+
+//Alterando dados
+$usuario = new Usuario();
+$usuario->loadByID(4);
+$usuario->update("Farmeti","merda");
+echo $usuario;
+
 ?>
